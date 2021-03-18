45487283a04ac33cc81c54522784bd9f018c3cd5
GagChicmic <gagchicmic@gmail.com>
2021-03-18 21:52:34 +0530

    Added validation only mobile left

--- src/Form.js
+++ src/Form.js
@@ -8,26 +8,23 @@ const Form = ({ fields, data, setData, handleSubmit, isSignUp }) => {
   // here field[isvalid] is used to check the validation onChange
   // field[isvalid] == true means input is valid otherwise invalid input type
   const checkValidation = (type, field, text) => {
-    // NAME VALIDATIONS
-    if (type === "first name") {
-      if (text.length < 6 || /\d/.test(text)) {
+    if (type == "first name") {
+      if (text.length < 6 || !/^[A-Za-z]+$/.test(text)) {
         field["isvalid"] = false;
         field["error"] =
           "length should be greater than 6 and use only alphabets";
       } else {
         field["isvalid"] = true;
       }
-    } else if (type == "last name" || /\d/.test(text)) {
-      if (text.length < 6 || /\d/.test(text)) {
+    } else if (type == "last name") {
+      if (text.length < 6 || !/^[A-Za-z]+$/.test(text)) {
         field["isvalid"] = false;
         field["error"] =
           "length should be greater than 6 and use only alphabets";
       } else {
         field["isvalid"] = true;
       }
-    }
-    // EMAIL AND PASSWORD VALIDATION
-    else if (type === "email") {
+    } else if (type === "email") {
       if (text.length == 0) {
         field["isvalid"] = true;
       } else {
@@ -52,13 +49,36 @@ const Form = ({ fields, data, setData, handleSubmit, isSignUp }) => {
     }
     // CONTACT VALIDATION
     else if (type === "contact") {
-      field["isvalid"] = /^\+(?:[0-9] ?){6,14}[0-9]$/.test(text);
+      if (text[0] !== "+") {
+        text = "+" + text;
+      }
+      if (!/^[+]91(9|8|7)\d{9}$/.test(text)) {
+        field["isvalid"] = false;
+        field["error"] = "enter valid mobile no";
+      } else {
+        field["isvalid"] = true;
+      }
     }
   };
 
   // handle the Onchange event
   const handleChange = (idx, text, type, field) => {
     // update the value onChange
+    if (type === "contact" && !/^[0-9]*$/.test(text)) {
+      return;
+    }
+    if (type === "DOB") {
+      const today = new Date();
+      const [year1, month1, day1] = text.split("-");
+      const [day2, month2, year2] = today.toLocaleDateString().split("/");
+      if (year1 > year2) {
+        return;
+      } else if (year1 == year2 && month1 > month2) {
+        return;
+      } else if (year1 == year2 && month1 == month2 && day1 > day2) {
+        return;
+      }
+    }
     const tempData = [...data];
     tempData[idx] = text;
     setData(tempData);
@@ -75,7 +95,7 @@ const Form = ({ fields, data, setData, handleSubmit, isSignUp }) => {
         {/* getting name and value from fieldData depend upon form type */}
         {/* <label htmlFor={field["name"]}>{field["name"]}</label> */}
         <input
-          style={{ outline: "none" }}
+          style={{}}
           type={field["type"]}
           id={field["name"]}
           name={field["name"]}
